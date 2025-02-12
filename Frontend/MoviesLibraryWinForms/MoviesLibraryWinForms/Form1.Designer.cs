f8ca3c2405ae96e25c099bb566a26786c6f3da86
Mikołaj Kocik <mikolajkocik@onet.pl>
2025-02-12 15:49:37 +0100

    DataGridViewButtonColumn - edit/delete, setup

--- Frontend/MoviesLibraryWinForms/MoviesLibraryWinForms/Form1.Designer.cs
+++ Frontend/MoviesLibraryWinForms/MoviesLibraryWinForms/Form1.Designer.cs
@@ -29,15 +29,13 @@
         private void InitializeComponent()
         {
             btnAdd = new Button();
-            dataGridView1 = new DataGridView();
-            btnEdit = new Button();
-            btnDelete = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            dataGridViewMovies = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewMovies).BeginInit();
             SuspendLayout();
             // 
             // btnAdd
             // 
-            btnAdd.Location = new Point(28, 21);
+            btnAdd.Location = new Point(44, 93);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(101, 33);
             btnAdd.TabIndex = 1;
@@ -45,54 +43,31 @@
             btnAdd.UseVisualStyleBackColor = true;
             btnAdd.Click += btnAdd_Click;
             // 
-            // dataGridView1
+            // dataGridViewMovies
             // 
-            dataGridView1.Location = new Point(28, 74);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(659, 344);
-            dataGridView1.TabIndex = 2;
-            // 
-            // btnEdit
-            // 
-            btnEdit.Location = new Point(693, 93);
-            btnEdit.Name = "btnEdit";
-            btnEdit.Size = new Size(101, 33);
-            btnEdit.TabIndex = 3;
-            btnEdit.Text = "Edit";
-            btnEdit.UseVisualStyleBackColor = true;
-            btnEdit.Click += btnEdit_Click;
-            // 
-            // btnDelete
-            // 
-            btnDelete.Location = new Point(693, 168);
-            btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(101, 33);
-            btnDelete.TabIndex = 4;
-            btnDelete.Text = "Delete";
-            btnDelete.UseVisualStyleBackColor = true;
-            btnDelete.Click += btnDelete_Click;
+            dataGridViewMovies.Location = new Point(44, 154);
+            dataGridViewMovies.Name = "dataGridViewMovies";
+            dataGridViewMovies.Size = new Size(737, 344);
+            dataGridViewMovies.TabIndex = 2;
+            dataGridViewMovies.CellContentClick += dataGridViewMovies_CellContentClick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(btnDelete);
-            Controls.Add(btnEdit);
+            ClientSize = new Size(858, 522);
             Controls.Add(btnAdd);
-            Controls.Add(dataGridView1);
+            Controls.Add(dataGridViewMovies);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewMovies).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private DataGridView dataGridViewMovies;
         private Button btnAdd;
-        private Button btnEdit;
-        private Button btnDelete;
     }
 }
--- Frontend/MoviesLibraryWinForms/MoviesLibraryWinForms/Form1.cs
+++ Frontend/MoviesLibraryWinForms/MoviesLibraryWinForms/Form1.cs
@@ -11,6 +11,7 @@ namespace MoviesLibraryWinForms
         public Form1()
         {
             InitializeComponent();
+            SetupDataGridViewButtons();
             _httpClient = new HttpClient();
         }
 
@@ -24,11 +25,11 @@ namespace MoviesLibraryWinForms
             try
             {
                 var movies = await _httpClient.GetFromJsonAsync<List<Movie>>(ApiUrl);
-                dataGridView1.DataSource = movies;
+                dataGridViewMovies.DataSource = movies;
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error during load data" + ex.Message);
+                MessageBox.Show("Error while loading data" + ex.Message);
             }
         }
 
@@ -46,20 +47,87 @@ namespace MoviesLibraryWinForms
                     }
                     else
                     {
-                        MessageBox.Show("Error while adding movie");
+                        MessageBox.Show("Error while adding data");
                     }
                 }
             }
         }
 
-        private async void btnEdit_Click(object sender, EventArgs e)
+        private void SetupDataGridViewButtons()
         {
-            //
+            if (!dataGridViewMovies.Columns.Contains("EditButton"))
+            {
+                DataGridViewButtonColumn editButtonColumn = new DataGridViewButtonColumn
+                {
+                    Name = "EditButton",
+                    HeaderText = "Edit",
+                    Text = "Edit",
+                    UseColumnTextForButtonValue = true
+                };
+
+                dataGridViewMovies.Columns.Add(editButtonColumn);
+            }
+
+            if (!dataGridViewMovies.Columns.Contains("DeleteButton"))
+            {
+                DataGridViewButtonColumn deleteButtonColumn = new DataGridViewButtonColumn
+                {
+                    Name = "DeleteButton",
+                    HeaderText = "Delete",
+                    Text = "Delete",
+                    UseColumnTextForButtonValue = true
+                };
+                
+                dataGridViewMovies.Columns.Add(deleteButtonColumn);
+            }
         }
 
-        private async void btnDelete_Click(object sender, EventArgs e)
+        private async void dataGridViewMovies_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            //
+            if (e.RowIndex < 0) return;
+
+            string columnName = dataGridViewMovies.Columns[e.ColumnIndex].Name;
+            Movie movie = (Movie)dataGridViewMovies.Rows[e.RowIndex].DataBoundItem;
+
+            if (movie == null) return;
+
+            if (columnName == "EditButton")
+            {
+                using (FormMovie modalForm = new FormMovie())
+                {
+                    if (modalForm.ShowDialog() == DialogResult.OK)
+                    {
+                        var response = await _httpClient.PutAsJsonAsync($"{ApiUrl}/{movie.Id}", movie);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            await LoadMoviesAsync();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error while editing data");
+                        }
+                    }
+                }
+            }
+            else if (columnName == "DeleteButton")
+            {
+                var confirm = MessageBox.Show("Are you sure that you want to delete the data?", "Confirmation", MessageBoxButtons.YesNo);
+
+                if (confirm == DialogResult.Yes)
+                {
+                    var response = await _httpClient.DeleteAsync($"{ApiUrl}/{movie.Id}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        await LoadMoviesAsync();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error while removing data");
+                    }
+                }
+            }
         }
     }
 }
